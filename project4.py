cecdd50cdea4064d12550890b873b4f0e717a825
Nisha <nishaadatiya132@gmail.com>
2026-04-01 14:43:22 +0530

    project4

--- project4.py
+++ project4.py
@@ -1,28 +1,59 @@
-print("Welcome to the Pattern Generator and Number Analyzer!")
-
+print("  Welcome to Pattern Generator & Number Analyzer ")
 
 while True:
-    print("Select an Option")
-    print("1. Generate a pattern")
-    print("2. Analyze a Range of Numbers")
+    print("\nSelect an Option:")
+    print("1. Generate a Pattern")
+    print("2. Analyze Range of Numbers")
     print("3. Exit")
     
-    choice =int( input("Enter your choice :"))
+    choice = input("Enter your choice : ")
     
-    match choice:
-        case 1:
-            print("Pattern")
-            for i in range(1,5+1):
-                for j in range(i):
-                    print("*",end=" ")
-                print()
-                    
+    if choice == "1":
+        while True:
+            rows = int(input("Enter the number of rows of pattern : "))
+            
+            if rows <= 0:
+                print("Invalid row count! Must be positive.")
+                break
+            else:
+                print("\nGenerated Pattern:\n")
                 
-        case 2:
-            pass
-        case 3:
-             pass
+                for i in range(1, rows + 1):
+                    for j in range(i):
+                        print("*", end=" ")
+                    print()
+                break
 
+    elif choice == "2":
+        while True:
+            start = int(input("Enter the start num of range: "))
+            end = int(input("Enter the end num of range : "))
+            
+            if end <= start:
+                print("Invalid range! End number must be greater than start.")
+                continue
+            else:
+                break
+        
+        total_sum = 0
+        
+        for num in range(start, end + 1):
+            
+            if num % 2 == 0:
+                print(num, "is Even")
+            else:
+                print(num, "is Odd")
+            
+            total_sum += num
+        
+        print("\nSum of all numbers =", total_sum)
+
+    elif choice == "3":
+        print("Exiting the program Goodbye!!")
+        break
+    
+    else:
+        print("Invalid choice! Please select 1, 2 or 3.")
 
     
 
